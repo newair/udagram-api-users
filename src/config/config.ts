e24c1b843f8383fe0b4096c98b0d8ffaadb2914e
Ishara <newair007@gmail.com>
2020-10-11 13:00:11 +1030

    adding service and deployment with screen shots

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,9 +4,9 @@ export const config = {
   'database': process.env.POSTGRES_DB,
   'host': process.env.POSTGRES_HOST,
   'dialect': 'postgres',
-  'aws_region': 'us-east-1',
-  'aws_profile': 'udacity-user',
-  'aws_media_bucket': 'udacity-monolith',
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
   'url': 'http://localhost:8100',
   'jwt': {
     'secret': process.env.JWT_SECRET,
